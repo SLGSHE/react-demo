2683d0e34d007e29cf7cacaaf06d4f4bff0d10ac
lingang <lingang@qq.com>
2019-06-14 20:06:06 +0800

    '更新'

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import Login from './pages/login/login'
 import Admin from './pages/admin/admin'
 
 export default class App extends Component {
+
   render() {
     return (
       <BrowserRouter>
         <Switch>
+          {/* 测试新建分支 master专用*/}
           <Route path='/login' component={Login}></Route>
           <Route path='/' component={Admin}></Route>
         </Switch>
