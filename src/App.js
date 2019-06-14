166f9daf145a5d2cc6fa1c7fc1ab2d7fb6893fc8
lingang <lingang@qq.com>
2019-06-14 19:49:03 +0800

    Update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react'
-import { Button } from 'antd';
+import { HashRouter, BrowserRouter, Switch, Route } from 'react-router-dom'
+
+import Login from './pages/login/login'
+import Admin from './pages/admin/admin'
 
 export default class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Button type="primary">Button</Button>
-
-      </div>
+      <BrowserRouter>
+        <Switch>
+          <Route path='/login' component={Login}></Route>
+          <Route path='/admin' component={Admin}></Route>
+        </Switch>
+      </BrowserRouter>
     )
   }
 } 
